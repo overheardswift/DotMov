d85b6511347989d94434486dea3a3054657c6d78
BK <bayu.kurniawan@kitabisa.com>
2023-07-29 15:03:21 +0700

    `RemoteNowPlayingLoaderTests`: Test execute requests data from remote on each call

--- DMNowPlayingiOS/DMNowPlayingTests/LoadNowPlayingFromRemoteUseCase.swift
+++ DMNowPlayingiOS/DMNowPlayingTests/LoadNowPlayingFromRemoteUseCase.swift
@@ -25,6 +25,18 @@ class RemoteNowPlayingLoaderTests: XCTestCase {
         
         XCTAssertEqual(client.requestedURLs, [expectedURL])
     }
+    
+    func test_execute_requests_data_from_remote_on_each_call() {
+        let request = PagedNowPlayingRequest(page: 1)
+        let expectedURL = makeURL("https://some-remote-url.dev/3/movie/now_playing?language=\(request.language)&page=\(request.page)")
+        let baseURL = makeURL("https://some-remote-url.dev")
+        let (sut, client) = makeSUT(baseURL: baseURL)
+        
+        sut.execute(request) { _ in }
+        sut.execute(request) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [expectedURL, expectedURL])
+    }
 }
 
 private extension RemoteNowPlayingLoaderTests {

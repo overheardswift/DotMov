6960adaba426887175b6b5e443852727c558fc76
BK <bayu.kurniawan@kitabisa.com>
2023-07-30 22:37:01 +0700

    Create new `DMMovieDetail` module

---
+++ DMMovieDetail/DMMovieDetail/DMMovieDetail.h
@@ -0,0 +1,18 @@
+//
+//  DMMovieDetail.h
+//  DMMovieDetail
+//
+//  Created by Bayu Kurniawan on 30/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DMMovieDetail.
+FOUNDATION_EXPORT double DMMovieDetailVersionNumber;
+
+//! Project version string for DMMovieDetail.
+FOUNDATION_EXPORT const unsigned char DMMovieDetailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMMovieDetail/PublicHeader.h>
+
+

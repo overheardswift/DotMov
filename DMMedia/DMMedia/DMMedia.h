a0e8d3926925f15a48961aa32af69b5130275e82
BK <bayu.kurniawan@kitabisa.com>
2023-07-30 17:30:49 +0700

    Create `DMMedia` module

---
+++ DMMedia/DMMedia/DMMedia.h
@@ -0,0 +1,18 @@
+//
+//  DMMedia.h
+//  DMMedia
+//
+//  Created by Bayu Kurniawan on 30/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DMMedia.
+FOUNDATION_EXPORT double DMMediaVersionNumber;
+
+//! Project version string for DMMedia.
+FOUNDATION_EXPORT const unsigned char DMMediaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMMedia/PublicHeader.h>
+
+
